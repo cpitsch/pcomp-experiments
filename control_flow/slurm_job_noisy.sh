f5f4aa8eebd2e2e69954569b6cc44f767d1128bd
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2024-10-20 22:43:13 +0200

    Add slurm job script for noisy logs

---
+++ control_flow/slurm_job_noisy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+
+### Job Parameters
+#SBATCH --job-name=pcomp_noisy_cf
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=c23ms
+#SBATCH --output=control_flow_output.txt
+
+### Program Code
+cd ~/jupyterlab/pcomp_experiments/control_flow
+source ../.venv/bin/activate
+
+python run_noisy.py --cores ${SLURM_JOB_CPUS_PER_NODE}
