6798ef0b2da6d3169e09674a7d3f2321a993893f
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2024-12-03 03:13:20 +0100

    Add slurm job script for FPR analysis

---
+++ road_traffic_random_splits/slurm_job.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/zsh
+
+### Job Parameters
+#SBATCH --job-name=pcomp_random_splits
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=c23ms
+#SBATCH --output=random_splits_output.txt
+
+### Program Code
+cd ~/pcomp-experiments/road_traffic_random_splits
+source ../.venv/bin/activate
+
+python run.py --cores ${SLURM_JOB_CPUS_PER_NODE}
