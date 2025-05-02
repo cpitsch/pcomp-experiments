ff4b673b3a6b3327b9f32448716b085ffede9590
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2025-05-02 17:24:06 +0200

    Merge control flow table generation into single script
    Also fix bug (?) where the dataframe is not filtered before calling
    `create_per_change_pattern_table`.

--- control_flow/generate_latex_tables.py
+++ control_flow/generate_latex_tables.py
@@ -1,28 +1,20 @@
-"""
-Largely the same logic as the other create_latex_tables, except different input directory
-and need to filter the dataframe. When the run.py and run_noisy.py are merged into one,
-and the outputs are together, the table gen scripts can also easily be merged
-"""
-
 from pathlib import Path
 from typing import Any, Literal, cast
 
 import pandas as pd
-from run_noisy import LogInstance, get_all_log_instances
+from run import LogInstance, get_all_log_instances
 from streamlit_analysis.main import (
     get_change_pattern_performance_table,
     get_performance_table,
     log_name_to_change_pattern,
     ostovar_to_change_pattern,
 )
+from tqdm.auto import tqdm
 
-RESULTS_BASE_DIR = Path("control_flow_results_noisy")
-
-
+RESULTS_BASE_DIR = Path("control_flow_results")
 SUMMARY_PATH = RESULTS_BASE_DIR / "summary.csv"
-LATEX_FILES_BASE_DIR = Path("latex_tables_noisy")
-
 FLOAT_FORMATTER = "{:.3g}".format
+LATEX_FILES_BASE_DIR = Path("generated_latex_tables")
 
 short_to_long = {v: k for k, v in ostovar_to_change_pattern.items()}
 short_to_long["bose"] = "Bose Log"
@@ -31,12 +23,24 @@ short_to_long |= {
     "cd": "Synchronize",
     "cp": "Copy",
     "re": "Add/Remove",
+    "ior": "IOR",
+    "iro": "IRO",
+    "oir": "OIR",
+    "ori": "ORI",
+    "rio": "RIO",
+    "roi": "ROI",
 }
 
 Source = Literal["ostovar", "ceravolo"]
 
 
 def save_if_not_exists(text: str, path: Path):
+    """Write content to a path, only if it does not already exist.
+
+    Args:
+        text (str): The content to write to the file.
+        path (Path): The path to the file.
+    """
     if path.exists():
         return
     if not path.parent.exists():
@@ -48,6 +52,15 @@ def save_if_not_exists(text: str, path: Path):
 def get_results_df(
     noise_level: int | None = None, source: Source | None = None
 ) -> pd.DataFrame:
+    """Load the results dataframe and filter for noise level and source.
+
+    Args:
+        noise_level (int | None, optional): The noise level to filter for. Defaults to None (no filtering).
+        source (Source | None, optional): The log source to filter for. Defaults to None (no filtering).
+
+    Returns:
+        pd.DataFrame: The results dataframe, filtered by source and noise level, if applicable.
+    """
     df = pd.read_csv(SUMMARY_PATH)
     df["change_pattern"] = df[["log_source", "log_name"]].apply(
         lambda row: log_name_to_change_pattern(row["log_source"], row["log_name"]),
@@ -64,6 +77,15 @@ def get_results_df(
 def get_splitted_results_df(
     noise_level: int | None = None, source: Source | None = None
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Load the results and split by technique
+
+    Args:
+        noise_level (int | None, optional): The noise level to filter for. Defaults to None (no filtering).
+        source (Source | None, optional): The log source to filter for. Defaults to None(no filtering).
+
+    Returns:
+        tuple[pd.DataFrame, pd.DataFrame]: The dataframes from sources: ("Bootstrap Test", "Permutation Test")
+    """
     df = get_results_df(noise_level, source)
     return (
         cast(pd.DataFrame, df[df["technique"] == "Bootstrap Test"]),
@@ -72,17 +94,36 @@ def get_splitted_results_df(
 
 
 def log_instance_to_df_row(log_instance: LogInstance) -> dict[str, Any]:
+    """Convert a LogInstance to a dict used for a dataframe row. Contains the columns "Change Pattern"
+    and "Positive", where the latter indicates whether a change was present in that instance.
+
+
+    Args:
+        log_instance (LogInstance): The LogInstance to convert.
+
+    Returns:
+        dict[str, Any]: The dataframe row
+    """
     return {
         "Change Pattern": log_name_to_change_pattern(
-            log_instance.source, log_instance.path.name.split(".")[0]  # type: ignore
+            log_instance.source,  # type: ignore
+            log_instance.path.name.split(".")[0],  # type: ignore
         ),
         "Positive": log_instance.has_drift,
     }
 
 
 def create_change_pattern_counts_table(
-    source: Source | None = None, noise_level: int | None = None
+    base_dir: Path, noise_level: int | None = None, source: Source | None = None
 ):
+    """Create the latex table showing how many positive/negative instances of each change pattern
+    there are. Optionally, can filter by source and noise level.
+
+    Args:
+        base_dir (Path): The base directory where latex files are saved.
+        noise_level (int | None, optional): The noise level to filter for. Defaults to None (no filtering).
+        source (Source | None, optional): The source to filter for. Defaults to None (no filtering).
+    """
     all_log_instances = [
         instance
         for instance in get_all_log_instances()
@@ -135,7 +176,7 @@ def create_change_pattern_counts_table(
         .replace(r"\bottomrule", r"\hline")
     )
     path = (
-        LATEX_FILES_BASE_DIR
+        base_dir
         / (source or "general")
         / (str(noise_level or "all"))
         / "change_pattern_logs.tex"
@@ -144,7 +185,7 @@ def create_change_pattern_counts_table(
 
 
 def create_general_metrics_table(
-    noise_level: int | None = None, source: Source | None = None
+    base_dir: Path, noise_level: int | None = None, source: Source | None = None
 ):
     bootstrap_df, permutation_test_df = get_splitted_results_df(noise_level, source)
 
@@ -182,15 +223,20 @@ def create_general_metrics_table(
         .replace(r"\bottomrule", r"\hline")
     )
     path = (
-        LATEX_FILES_BASE_DIR
-        / (source or "general")
-        / str(noise_level)
-        / "control_flow_results.tex"
+        base_dir / (source or "general") / str(noise_level) / "control_flow_results.tex"
     )
     save_if_not_exists(latex, path)
 
 
-def get_per_change_pattern_table(df: pd.DataFrame):
+def get_per_change_pattern_table(df: pd.DataFrame) -> pd.DataFrame:
+    """Get a dataframe listing the performance of the approaches on each change pattern.
+
+    Args:
+        df (pd.DataFrame): The results dataframe.
+
+    Returns:
+        pd.DataFrame: The per-change-pattern performance dataframe
+    """
     change_pattern_performance_table = get_change_pattern_performance_table(df)
 
     def descriptive_change_pattern(short_pattern: str):
@@ -209,6 +255,7 @@ def get_per_change_pattern_table(df: pd.DataFrame):
 
 
 def create_per_change_pattern_table(
+    base_dir: Path,
     df: pd.DataFrame,
     filename: str,
     noise_level: int,
@@ -227,27 +274,49 @@ def create_per_change_pattern_table(
         .replace(r"\midrule", r"\hline")
         .replace(r"\bottomrule", r"\hline")
     )
-    path = LATEX_FILES_BASE_DIR / source / str(noise_level) / filename
+    path = base_dir / source / str(noise_level) / filename
     save_if_not_exists(latex, path)
 
 
 if __name__ == "__main__":
-    sources = get_results_df()["log_source"].unique().tolist()
+    if not LATEX_FILES_BASE_DIR.exists():
+        LATEX_FILES_BASE_DIR.mkdir()
+
+    total_df = get_results_df()
+    num_combinations = total_df[["log_source", "noise_level"]].drop_duplicates().shape[0]
+    num_combinations += total_df["noise_level"].nunique() # Add all None, x pairs
+    progress = tqdm(
+        total= num_combinations * 4,
+        desc="Creating latex tables",
+    )
+
+    sources = total_df["log_source"].unique().tolist()
     for source in sources + [None]:
         noise_levels = get_results_df(None, source)["noise_level"].unique()
         for noise_level in noise_levels:
-            create_change_pattern_counts_table(source, noise_level)
-            create_general_metrics_table(noise_level, source)
-            bootstrap_df, permutation_df = get_splitted_results_df(noise_level)
+            create_change_pattern_counts_table(
+                LATEX_FILES_BASE_DIR, noise_level, source
+            )
+            progress.update()
+            create_general_metrics_table(LATEX_FILES_BASE_DIR, noise_level, source)
+            progress.update()
+            bootstrap_df, permutation_df = get_splitted_results_df(
+                noise_level, source
+            )
             create_per_change_pattern_table(
+                LATEX_FILES_BASE_DIR,
                 bootstrap_df,
                 "bootstrap_test_per_change_pattern.tex",
                 noise_level,
                 source or "general",
             )
+            progress.update()
             create_per_change_pattern_table(
+                LATEX_FILES_BASE_DIR,
                 permutation_df,
                 "permutation_test_per_change_pattern.tex",
                 noise_level,
                 source or "general",
             )
+            progress.update()
+    progress.close()
